83730e1b66b5a341a9532b958fb61aad488fe3d0
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-06-09 19:44:29 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirims2117/mirim_project_0609_1/MainActivity.java
@@ -0,0 +1,22 @@
+package kr.hs.emirims2117.mirim_project_0609_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] items={"CSI-Newyork", "CSI-Lasvegas", "CSI-Miami","Friends"
+                ,"Fringe","Lost","Lost-Island"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, items);
+        AutoCompleteTextView auto=findViewById(R.id.auto);
+        auto.setAdapter(adapter);
+
+    }
+}
